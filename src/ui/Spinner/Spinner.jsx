b4d04065dea25bf3c9af9a9a581ce156bb0ec8d4
davidemmt <92088072+davidemmt@users.noreply.github.com>
2022-01-23 19:37:48 +0000

    refactored spinner accessibility

--- src/ui/Spinner/Spinner.jsx
+++ src/ui/Spinner/Spinner.jsx
@@ -4,7 +4,11 @@ import styles from './Spinner.module.css';
 const Spinner = ({
   className = ''
 }) => (
-    <div className={`${styles.Spinner} ${styles[className]}`}>Loading...</div>
+    <div
+      role='alert'
+      aria-label='loading'
+      className={`${styles.Spinner} ${styles[className]}`}
+    />
 )
 
 export default Spinner;
